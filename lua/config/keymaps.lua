68f7327c4c50c58ca7eeae26dc01a2c8eaf5c186
Pundima-Lakshan <pundimal@gmail.com>
2025-03-13 08:24:02 +0530

    save buffer content shortcut

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,17 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+
+function Map(mode, lhs, rhs, opts)
+  local options = { noremap = true, silent = true }
+
+  -- Extend the options with `which_key` description if provided
+  if opts then
+    options = vim.tbl_extend("force", options, opts)
+  end
+
+  vim.keymap.set(mode, lhs, rhs, options)
+end
+
+Map("n", "<leader>bs", ":w<CR>", { desc = "Save buffer content to file" })
