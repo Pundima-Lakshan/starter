569dbdfecfc730e0a5d241b04d64d8788a1a85c0
Pundima-Lakshan <pundimal@gmail.com>
2025-03-28 14:03:49 +0530

    plugins updates

--- lua/plugins/plugs.lua
+++ lua/plugins/plugs.lua
@@ -1,15 +1,37 @@
 -- every spec file under the "plugins" directory will be loaded automatically by lazy.nvim
+
 --
 -- In your plugin files, you can:
 -- * add extra plugins
 -- * disable/enabled LazyVim plugins
 -- * override the configuration of LazyVim plugins
 return {
-  -- change trouble config
+  {
+    "akinsho/bufferline.nvim",
+    enabled = false,
+  },
+  {
+    "nvim-neo-tree/neo-tree.nvim",
+    opts = {
+      window = {
+        position = "right",
+      },
+    },
+  },
   {
     "folke/trouble.nvim",
     -- opts will be merged with the parent spec
     opts = { use_diagnostic_signs = true },
+  },
+  {
+    "folke/snacks.nvim",
+    -- opts will be merged with the parent spec
+    opts = {
+      picker = {
+        files = { hidden = true },
+        bufferes = { hidden = true },
+      },
+    },
   },
   {
     "christoomey/vim-tmux-navigator",
